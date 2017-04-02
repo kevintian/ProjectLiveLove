5db3a5654cb9d4e4294b9a9111134d6316bea6b4
nineeshakoshy124 <nineesha@gmail.com>
2017-04-02 12:34:54 -0400

    added registration.php and registration.js

---
+++ js/registration.js
@@ -0,0 +1,27 @@
+$(function () {
+    $("#registrationSubmit").click(function(){
+        $.ajax({
+            type: 'POST',
+        url: '/ProjectLiveLove/php/registration.php', // use .. to "go up" (out of the folder)
+        data: {
+            username: $("#usernamesu").val(), //hashtag means id, . means class 
+            firstname: $("#firstname").val(),
+            lastname: $("#lastname").val(), //getting the values from what was inputted in the form 
+            email: $("#emailsu").val(),
+            password: $("#passwordsu").val()
+        },
+        success : function(data) {
+            alert(data)
+
+        },
+
+        error: function (xhr, ajaxOptions, thrownError) {
+        alert(xhr.status);
+        alert(thrownError);
+      }
+    });
+    });
+});
+
+    //when someone clicks register, take what they inputted and make it into an object that can be sent to the php server 
+
