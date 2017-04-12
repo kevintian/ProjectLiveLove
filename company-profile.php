377fe59948fd49e11bc5adeb78399bbcab6a75d5
kevitian <kevin.tian@cox.com>
2017-04-12 10:31:50 -0400

    Updated profile pages to pull info from database

--- company-profile.php
+++ company-profile.php
@@ -1,3 +1,17 @@
+<?php
+//Get the relevant data from the database for this company
+
+$stmt = $dbc->prepare('SELECT * FROM accounts INNER JOIN companies ON accounts.username = companies.username WHERE accounts.username = ?');
+//s means string
+$stmt->bind_param('s', $query['id']);
+$stmt->execute();
+
+$response = $stmt->get_result();
+
+$row = mysqli_fetch_assoc($response); //Gets the first (and only) row as an associative array
+
+
+?>
 <div class="padding" style="padding-top: 50px;">
 
 </div>
@@ -11,7 +25,9 @@
                         <img class="img-thumbnail img-fluid" src="https://lut.im/7JCpw12uUT/mY0Mb78SvSIcjvkf.png"
                              width="300px" height="300px">
                         <div class="profile-info">
-                            <h3 class="card-title">Company Name</h3>
+                            <h3 class="card-title">
+                                <?php echo htmlspecialchars($row['companyName']); ?>
+                            </h3>
                             <h6 class="card-subtitle mb-2 text-muted">Organization</h6>
                             <span class="badge badge-pill badge-info">3 Current Events</span>
                         </div>
@@ -26,16 +42,9 @@
                                 <a id="editProfile" href="#">Edit</a>
                             </div>
                         </div>
-                        Google was founded in 1996 by Larry Page and Sergey Brin while they were Ph.D. students at
-                        Stanford University, in California. Together, they own about 14 percent of its shares and
-                        control 56 percent of the stockholder voting power through supervoting stock. They incorporated
-                        Google as a privately held company on September 4, 1998. An initial public offering (IPO) took
-                        place on August 19, 2004, and Google moved to its new headquarters in Mountain View, California,
-                        nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various
-                        interests as a conglomerate called Alphabet Inc. Google, Alphabet's leading subsidiary, will
-                        continue to be the umbrella company for Alphabet's Internet interests. Upon completion of the
-                        restructure, Sundar Pichai became CEO of Google, replacing Larry Page, who became CEO of
-                        Alphabet.
+                        <?php
+                            echo htmlspecialchars($row['companyDescription'])
+                        ?>
                     </div>
                 </div>
             </div>
--- user-profile.php
+++ user-profile.php
@@ -1,3 +1,18 @@
+<?php
+//Get the relevant data from the database for this company
+
+$stmt = $dbc->prepare('SELECT * FROM accounts INNER JOIN users ON accounts.username = users.username WHERE accounts.username = ?');
+//s means string
+$stmt->bind_param('s', $query['id']);
+$stmt->execute();
+
+$response = $stmt->get_result();
+
+$row = mysqli_fetch_assoc($response); //Gets the first (and only) row as an associative array
+
+
+?>
+
 <div class="padding" style="padding-top: 50px;">
 
 </div>
@@ -11,9 +26,13 @@
                         <img class="img-thumbnail img-fluid" src="https://lut.im/7JCpw12uUT/mY0Mb78SvSIcjvkf.png"
                              width="300px" height="300px">
                         <div class="profile-info">
-                            <h3 class="card-title">Kevin Tian</h3>
+                            <h3 class="card-title">
+                                <?php echo htmlspecialchars($row['firstName']." ".$row['lastName']); ?>
+                            </h3>
                             <h6 class="card-subtitle mb-2 text-muted">Volunteer</h6>
-                            <span class="badge badge-danger">0 Points</span>
+                            <span class="badge badge-danger">
+                                <?php echo htmlspecialchars($row['points']); ?>
+                            </span>
                             <div style="padding-top: 5px;">
                                 <span class="badge badge-pill badge-info">Poverty & Relief</span>
                                 <span class="badge badge-pill badge-success">Environmentalism</span>
@@ -30,16 +49,7 @@
                                 <a id="editProfile" href="#">Edit</a>
                             </div>
                         </div>
-                        Google was founded in 1996 by Larry Page and Sergey Brin while they were Ph.D. students at
-                        Stanford University, in California. Together, they own about 14 percent of its shares and
-                        control 56 percent of the stockholder voting power through supervoting stock. They incorporated
-                        Google as a privately held company on September 4, 1998. An initial public offering (IPO) took
-                        place on August 19, 2004, and Google moved to its new headquarters in Mountain View, California,
-                        nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various
-                        interests as a conglomerate called Alphabet Inc. Google, Alphabet's leading subsidiary, will
-                        continue to be the umbrella company for Alphabet's Internet interests. Upon completion of the
-                        restructure, Sundar Pichai became CEO of Google, replacing Larry Page, who became CEO of
-                        Alphabet.
+                        <?php echo htmlspecialchars($row['userBio']); ?>
                     </div>
                 </div>
             </div>
@@ -55,39 +65,8 @@
                         </div>
                     </div>
                     <hr>
-                    <div class="card event-listing">
-                        <div class="card-block">
-                            <div class="row">
-                                <div class="col-lg-2">
-                                    <img src="https://www.w3schools.com/css/trolltunga.jpg" style='width:175px; height:150px; padding-right:0px;'
-                                         class="img-fluid rounded mx-auto d-block" alt="Responsive image">
-                                </div>
-                                <div class="col-lg-10">
-                                    <h4 class="card-title">Event 1</h4>
-                                    <h6 class="card-subtitle mb-2 text-muted">Organization Name</h6>
-                                    <p class="card-text">Event description</p>
-                                    <a href="#" data-toggle="button" class="btn btn-outline-success">Going</a>
-                                    <a href="#" data-toggle="button" class="btn btn-outline-primary">Not Interested</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card event-listing">
-                        <div class="card-block">
-                            <div class="row">
-                                <div class="col-lg-2">
-                                    <img src="https://www.w3schools.com/css/trolltunga.jpg" style='width:175px; height:150px; padding-right:0px;'
-                                         class="img-fluid rounded mx-auto d-block" alt="Responsive image">
-                                </div>
-                                <div class="col-lg-10">
-                                    <h4 class="card-title">Event 2</h4>
-                                    <h6 class="card-subtitle mb-2 text-muted">Organization Name</h6>
-                                    <p class="card-text">Event description</p>
-                                    <a href="#" data-toggle="button" class="btn btn-outline-success">Going</a>
-                                    <a href="#" data-toggle="button" class="btn btn-outline-primary">Not Interested</a>
-                                </div>
-                            </div>
-                        </div>
+                    <div id = "result">
+                        <!--Handlebars will populate this with events. Template is below-->
                     </div>
                 </div>
             </div>
@@ -100,6 +79,28 @@
             </div>
         </div>
     </div>
+
+    <!-- Template for Handlebars -->
+    <script id="event-template" type="text/x-handlebars-template">
+        {{#each this}}
+        <div class="card event-listing">
+            <div class="card-block">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="img float-left"
+                             style="background-image: url('http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg'); margin-right:15px;">
+                        </div>
+                        <h4 class="card-title">{{Event_Name}}</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">{{Organization_Name}}</h6>
+                        <p class="card-text">{{Event_description}}</p>
+                        <a href="#" class="btn btn-outline-success">Going</a>
+                        <a href="#" class="btn btn-outline-primary">Not Interested</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{/each}}
+    </script>
 </div>
 
 <br>
